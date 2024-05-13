5748f93a33545fac21303ece89c31b23999d4f1e
Aziz <ngwk.zz@outlook.com>
2024-05-13 17:19:52 +0300

    MongoDB utils

--- utils/redis.js
+++ utils/redis.js
@@ -1,5 +1,5 @@
-const redis = require('redis');
-const { promisify } = require('util');
+import redis from 'redis';
+import { promisify } from 'util';
 
 class RedisClient {
   constructor() {
@@ -33,4 +33,4 @@ class RedisClient {
 }
 
 const redisClient = new RedisClient();
-module.exports = redisClient;
+export default redisClient;
